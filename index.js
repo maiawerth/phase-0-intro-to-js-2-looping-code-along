d9903944963663aae79a3b6104e76875f0e21a57
Maia Werth <maia.moo@me.com>
2024-02-25 01:06:36 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+    let message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+         messages.push(message);
+            }
+        debugger;
+            return messages;
+      }
+      let names = ["Rosie", "Shae", "Hannah", "Maia"];
+      let eventName = "birthday";
+
+      console.log(writeCards(names, eventName));
+
+      function countDown(number) {
+        while (number >= 0) {
+          console.log(number);
+          number--;
+        }
+      }
